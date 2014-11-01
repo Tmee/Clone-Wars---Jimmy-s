feb5b6c40dc617bb3d7081324461161bd6c76086
Timothy Mee <tjmee90@gmail.com>
2014-11-01 15:09:59 -0600

    initial commit

---
+++ lib/app.rb
@@ -0,0 +1,13 @@
+require 'bundler'
+Bundler.require
+
+class JimmysApp < Sinatra::Base
+  register Sinatra::Reloader
+  set :method_override, true
+  set :root, 'lib/app'
+
+
+  get '/' do
+    erb :index
+  end
+end
