25c07950330e6579ea5a0dd82196683542d4710f
Greg Narajka <gregnar@gmail.com>
2014-11-02 14:31:19 -0700

    working on menu stuff

---
+++ lib/app/menu_item.rb
@@ -0,0 +1,27 @@
+class MenuItem
+  attr_reader :url_id, :name, :price, :description
+
+  def initialize (data)
+    url_id      = data[:url_id]
+    name        = data[:name]
+    price       = data[:price]
+    description = data[:description]
+  end
+
+  def self.all
+  end
+
+  def database
+    @database ||= YAML::Store.new "ideabox"
+  end
+
+  def save
+  database.transaction do |db|
+    db['menu_items'] ||= []
+    db['menu_items'] << { url_id: url_id,
+                          name: name,
+                          price: price,
+                          description: description
+                        }
+  end
+end
