4ae2b06a0ac8851c6974de94576a9137c8389e5b
Konr Larson <larsonkonr@gmail.com>
2014-11-04 07:16:31 -0700

    trying to get mail to work v4

--- lib/app.rb
+++ lib/app.rb
@@ -6,7 +6,6 @@ class JimmysApp < Sinatra::Base
   register Sinatra::Reloader
   set :method_override, true
   set :root, 'lib/app'
-  attr_reader :username, :password
 
 
   get '/' do
@@ -37,53 +36,21 @@ class JimmysApp < Sinatra::Base
     puts "Thank you for logging in"
     redirect '/'
   end
-  # post '/contact_us' do
-  #   name = params[:name]
-  #   subject = params[:subject] || ""
-  #   email = params[:mail]
-  #   message = params[:message]
-  #
-  #   Pony.mail(:to => 'larsonkonr@gmail.com',
-  #             :from => email,
-  #             :subject => subject,
-  #             :body => message)
-  # end
-# require 'mail'
-# Mail.defaults do
-#   delivery_method :smtp, { :address   => "smtp.sendgrid.net",
-#                            :port      => 587,
-#                            :domain    => "http://lodojimmys.herokuapp.com/",
-#                            :user_name => "larsonkonr",
-#                            :password  => "gameboy1",
-#                            :authentication => 'plain',
-#                            :enable_starttls_auto => true }
-# end
-# post '/message' do
-#   mail = Mail.deliver do
-#     to 'yourRecipient@domain.com'
-#     from "Your Name #{params[:name]}"
-#     subject 'This is the subject of your email'
-#     text_part do
-#       body 'Hello world in text'
-#     end
-#     html_part do
-#       content_type 'text/html; charset=UTF-8'
-#       body '<b>Hello world in HTML</b>'
-#     end
-#   end
-# end
 
-  # mail = SendGrid::Mail.new do |m|
-  #   m.to = 'larsonkonr@gmail.com'
-  #   m.from = :from
-  #   m.subject = :subject
-  #   m.text = :body
-  # end
+  post '/contact_us' do
+    name = params[:name]
+    subject = params[:subject] || ""
+    email = params[:mail]
+    message = params[:message]
+
+    Pony.mail(:to => 'larsonkonr@gmail.com',
+              :from => email,
+              :subject => subject,
+              :body => message)
+  end
+
   #
-  # post '/message' do
-  #   Pony.mail(:to => 'larsonkonr@gmail.com', :from => params[:name], :subject => 'Feedback from ' + params[:name], :body => params[:message])
-  #   redirect '/'
-  # end
+  #
   # post '/contact' do
   # require 'pony'
   # Pony.mail({
